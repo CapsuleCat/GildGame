545e9850a5f9cfe5419b9fcde3836b67c156e521
Ivan Montiel <idmontie@asu.edu>
2016-01-30 13:55:55 -0700

    Refactor Game view

---
+++ client/components/game/action-area/action-area.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {default as SummoningRing} from '../summoning-ring/summoning-ring.jsx';
+import {default as SummonButton} from '../summon-button/summon-button.jsx';
+import {default as Arena} from '../arena/arena.jsx';
+
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        TODO
+      </div>
+    );
+
+    // <SummoningRing/>
+    // <SummonButton/>
+    // <Arena/>
+  }
+});
--- client/stores/game-store.js
+++ client/stores/game-store.js
@@ -8,13 +8,22 @@ const GameStore = Reflux.createStore({
   listenables: [ GameActions ],
 
   init() {
+    this._wins = 0;
+    this._elements = [];
 
+    this._generateElements();
   },
 
   getInitialState() {
     return {
-
+      winCount: this._wins,
+      elements: this._elements
     };
+  },
+
+  _generateElements() {
+    // TODO pick 5 random elements
+    // and copy them into this._elements
   }
 });
 
