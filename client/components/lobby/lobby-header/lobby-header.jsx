f8104d1b590203e4121d0532e34aeaa3718b5cf7
Ivan Montiel <idmontie@asu.edu>
2016-01-30 21:03:29 -0700

    Generate names and dont allow empty usernames

--- client/components/lobby/lobby-header/lobby-header.jsx
+++ client/components/lobby/lobby-header/lobby-header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import generateName from 'sillyname';
 
 import {default as Game} from '../../game/game/game.jsx';
 
@@ -11,6 +12,10 @@ export default React.createClass({
     e.preventDefault();
 
     const username = this.refs.username.value;
+
+    if (username.trim() === '') {
+      return;
+    }
 
     LobbyActions.create(username, (gameId) => {
       GameActions.setGame(
@@ -29,6 +34,10 @@ export default React.createClass({
   },
 
   render() {
+    if (this.givenName == null) {
+      this.givenName = generateName();
+    }
+
     return (
       <div className="clearfix lobby__lobby-header">
         <div>
@@ -40,6 +49,7 @@ export default React.createClass({
           <input
               className="lobby__lobby-header--usernameInput"
               onChange={this.handleChange}
+              defaultValue={this.givenName}
               id="username"
               type="text"
               ref="username" />
