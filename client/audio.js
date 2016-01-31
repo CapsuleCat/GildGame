97408e726f262e40e5adcee76a51a51e2a11ac94
gustmes <gustmes@hotmail.com>
2016-01-31 12:06:33 -0700

    Button sounds

--- client/audio.js
+++ client/audio.js
@@ -4,7 +4,7 @@ let audio = {
   lose: new Audio('/sounds/lose_sound.mp3'),
   chant: new Audio('sounds/chant_sound.mp3'),
   elementIcon: new Audio('sounds/element_icon_sound.mp3'),
-  summonButton: new Audio ('sounds/summon_button_sound.mp3'),
+  summonButton: new Audio('sounds/summon_button_sound.mp3'),
 };
 
 export {audio};
